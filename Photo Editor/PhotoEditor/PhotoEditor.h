752eebf6a5bef637df66d38caf3241b8a2be96a7
Daniel Sanchez <dan@txtter.com>
2019-02-14 19:50:40 +0000

    PE-01: Creating PhotoEditor target framework

---
+++ Photo Editor/PhotoEditor/PhotoEditor.h
@@ -0,0 +1,19 @@
+//
+//  PhotoEditor.h
+//  PhotoEditor
+//
+//  Created by Daniel Sanchez on 14/02/2019.
+//  Copyright © 2019 Mohamed Hamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoEditor.
+FOUNDATION_EXPORT double PhotoEditorVersionNumber;
+
+//! Project version string for PhotoEditor.
+FOUNDATION_EXPORT const unsigned char PhotoEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoEditor/PublicHeader.h>
+
+
